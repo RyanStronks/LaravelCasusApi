7a16cf536cebae73072e60e8f1288a9c9b696deb
RyanStronks <ryan.stronks@student.graafschapcollege.nl>
2025-05-19 13:27:07 +0200

    try 50000

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,22 +9,52 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller {
     public function login(Request $request) {
+
+
+
         $credentials = $request->validate([
+
+
             'email' => 'required|email',
+
+
             'password' => 'required'
+
+
         ]);
 
+
+
+
+
         if (!Auth::attempt($credentials)) {
+
+
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
-        // Fetch the user directly from the database
-        $user = User::where('email', $credentials['email'])->firstOrFail();
+
+
+
+
+        $user = Auth::user();
+
+
         $token = $user->createToken('api-token')->plainTextToken;
 
+
+
+
+
         return response()->json([
+
+
             'user' => $user,
+
+
             'token' => $token
+
+
         ]);
     }
 
