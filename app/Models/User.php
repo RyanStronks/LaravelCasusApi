d247ccc033495ac75219bb3a0561c46ef0cbcf79
RyanStronks <ryan.stronks@student.graafschapcollege.nl>
2025-05-20 11:17:33 +0200

    Uuid support

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, Notifiable, HasFactory;
+    use HasApiTokens, HasUuids, Notifiable, HasFactory;
 
     /**
      * The attributes that are mass assignable.
