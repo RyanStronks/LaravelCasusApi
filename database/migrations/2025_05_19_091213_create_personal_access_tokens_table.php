2c6ca3c706f80985341f4a1997f73540e8ac712a
RyanStronks <ryan.stronks@student.graafschapcollege.nl>
2025-05-22 15:35:03 +0200

    bomboclaat

--- database/migrations/2025_05_19_091213_create_personal_access_tokens_table.php
+++ database/migrations/2025_05_19_091213_create_personal_access_tokens_table.php
@@ -10,8 +10,7 @@ return new class extends Migration {
      */
     public function up(): void {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->primary('id');
+            $table->uuid('id')->primary();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
