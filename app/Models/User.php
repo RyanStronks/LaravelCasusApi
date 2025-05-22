01c1ace9efe5d0ddced142348a1dc39e946c08e1
RyanStronks <ryan.stronks@student.graafschapcollege.nl>
2025-05-22 15:14:08 +0200

    bomboclaat

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,10 @@ class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasUuids, Notifiable, HasFactory;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+
     /**
      * The attributes that are mass assignable.
      *
